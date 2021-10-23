e9999fe40cf5d486983126cb2083d6f618c85b1b
aquinoa-nba <aquinoa@student.21-school.ru>
2021-10-23 18:50:21 +0300

    add private msg + color

--- Chat/Chat.hpp
+++ Chat/Chat.hpp
@@ -3,6 +3,7 @@
 #include "../Clients/Clients.hpp"
 #include "../Channel/Channel.hpp"
 
+
 class Chat
 {
 private:
@@ -41,9 +42,29 @@ public:
 	void 	printHelp(Clients &src, std::vector<string> &cmd);
 	void	quitClient(Clients &src, vector<string> &cmd);
 	void    changeNickname(Clients &src, vector<string> &cmd);
+	void    sendPrivateMessage(Clients &src, vector<string> &cmd);
 };
 
 bool 		checkNicknameAlreadyUsed(std::vector<Clients> &clients, string input);
 int			checkEmptyMessage(Clients &src);
 void		sendMessageToClient(Clients &src, string output);
 std::vector<std::string>	ft_split(std::string str, const std::string &del);
+
+# define	NO_COLOR	"\033[0m"
+
+# define	BLACK		"\033[0;30m"
+# define	RED			"\033[0;31m"
+# define	GREEN		"\033[0;32m"
+# define	YELLOW		"\033[0;33m"
+# define	BLUE		"\033[0;34m"
+# define	PURPLE		"\033[0;35m"
+# define	CYAN		"\033[0;36m"
+
+# define	B_GRAY		"\033[1;30m"
+# define	B_RED		"\033[1;31m"
+# define	B_GREEN		"\033[1;32m"
+# define	B_YELLOW	"\033[1;33m"
+# define	B_BLUE		"\033[1;34m"
+# define	B_PURPLE	"\033[1;35m"
+# define	B_CYAN		"\033[1;36m"
+# define	B_WHITE		"\033[1;37m"
